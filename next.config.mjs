0d740f1dc4004ad36b95505b13c0c27037dc6b00
dave-stronghold <davestronghold@hotmail.com>
2024-05-06 12:09:05 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  typescript:{
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withMDX(config);
